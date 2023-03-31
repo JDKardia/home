4daf0f53d52e2a2e95e52d6418af20dad0cd9a05
Joseph Kardia <kardia@stripe.com>
2023-03-31 16:10:14 -0500

    update bin and saved appscripts

--- appscript-saves/git-notif-filter.js
+++ appscript-saves/git-notif-filter.js
@@ -1,5 +1,4 @@
 function archiveGitNotifsThatArentRelevant() {
-  const notForMe = GmailApp.search('label:inbox label:Git -label:Git/Me',0,100);
-  const forMeButMerged = GmailApp.search('label:inbox label:Git/me Merged into master',0,100);
-  GmailApp.moveThreadsToArchive(notForMe.concat(forMeButMerged))
+  const merged = GmailApp.search('label:inbox label:Fin ',0,100);
+  GmailApp.moveThreadsToArchive(merged)
 };
